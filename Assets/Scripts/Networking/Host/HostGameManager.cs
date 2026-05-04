2abb018a082fa3f5870d4844c1e8c16bf97c1e64
Adrian <a25adrianrs@iessanclemente.net>
2026-05-04 10:20:30 +0200

    Base antes de 3.12

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -20,6 +20,8 @@ public class HostGameManager
     private string lobbyId;
 
     private const int MaxConnections = 20;
+    // Constante que almacena el nombre de la escena del juego, 
+    // para evitar errores de tipeo
     private const string GameSceneName = "Game by Adrián";
 
     public async Task StartHostAsync()
